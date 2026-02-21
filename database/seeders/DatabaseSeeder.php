4e4847fab7f08e3edf81520df403bab13509569c
bangameck <rahmad.looker@gmail.com>
2026-02-21 21:43:19 +0700

    Update Seeder ThemeColor for masjid digital by RadevankaProject v1.0.2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -195,5 +195,9 @@ class DatabaseSeeder extends Seeder
             'is_active' => 1,
             'created_at' => now(),
         ]);
+
+        $this->call([
+            ThemeColorSeeder::class,
+        ]);
     }
 }
